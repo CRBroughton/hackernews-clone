4bc4866c88b9f5a32661588b74053350f39ae390
CRBroughton <crbroughton@posteo.uk>
2022-01-16 11:24:44 +0000

    added placeholder login link

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="bg-orange-600 py-1">
-    <p class="text-gray-900 ml-2">
+  <div class="bg-orange-600 py-1 px-2 flex">
+    <p class="text-gray-900">
       Hackernews Clone
     </p>
+    <a href="" class="text-gray-900 ml-auto">
+      login
+    </a>
   </div>
 </template>
 
