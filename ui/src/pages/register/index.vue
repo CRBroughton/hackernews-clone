9a1769438a34b1843b9b61d94ce210a69161a40b
CRBroughton <crbroughton@posteo.uk>
2022-01-16 11:47:14 +0000

    added reactive obj to register

--- ui/src/pages/register/index.vue
+++ ui/src/pages/register/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <p class="text-gray-700">
-      Register
-    </p>
+  <div class="flex flex-col gap-1 w-60 p-2">
+    <input v-model="data.username" class="border border-2" type="text" placeholder="username">
+    <input v-model="data.email" class="border border-2" type="text" placeholder="email">
+    <input v-model="data.password" class="border border-2" type="text" placeholder="password">
+    <button class="border border-2">
+      Submit
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
+
+const data = reactive({
+  username: '',
+  email: '',
+  password: '',
+})
 
 </script>
 
