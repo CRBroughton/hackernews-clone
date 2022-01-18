a110a94a2a350e24e8d44735295b1fbffb568b7b
CRBroughton <crbroughton@posteo.uk>
2022-01-18 21:29:27 +0000

    added vite icons

--- ui/src/components/Post.vue
+++ ui/src/components/Post.vue
@@ -1,15 +1,5 @@
-<template>
-  <div class="flex flex-col text-gray-800 pb-2">
-    <h1 class="text-sm">
-      <a :href="props.url">{{ props.description }}</a> <a :href="props.url" class="hover:underline hover:cursor-pointer">{{ `(${props.url})` }}</a>
-    </h1>
-    <p class="text-xs text-gray-700">
-      {{ `${props.voters} points` }} {{ `Posted by ${props.postedBy.name}` }}
-    </p>
-  </div>
-</template>
-
 <script setup lang="ts">
+
 interface Props {
   description: string
   url: string
@@ -21,6 +11,20 @@ interface Props {
 
 const props = defineProps<Props>()
 </script>
+
+<template>
+  <div class="flex flex-col text-gray-800 pb-2">
+    <div class="flex">
+      <MdiMenuUp />
+      <h1 class="text-sm">
+        <a :href="props.url">{{ props.description }}</a> <a :href="props.url" class="hover:underline hover:cursor-pointer">{{ `(${props.url})` }}</a>
+      </h1>
+    </div>
+    <p class="text-xs text-gray-700 ml-1">
+      {{ `${props.voters} points` }} {{ `Posted by ${props.postedBy.name}` }}
+    </p>
+  </div>
+</template>
 
 <style scoped>
 
