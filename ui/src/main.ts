437c5de988f74328dc9e844701d14bdc18d730ad
CRBroughton <crbroughton@posteo.uk>
2022-01-20 21:44:31 +0000

    added vue-cookie-next to deps

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,15 +3,15 @@ import 'virtual:windi.css'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import urql from '@urql/vue'
+import { VueCookieNext } from 'vue-cookie-next'
 import App from './App.vue'
 import routes from '~pages'
-
 const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
 
-app.use(router).use(createPinia()).use(urql, {
+app.use(router).use(VueCookieNext).use(createPinia()).use(urql, {
   url: 'http://localhost:3000',
 }).mount('#app')
