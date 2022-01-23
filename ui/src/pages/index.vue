7a7b9bbfbc7df88330808f4d023442214f059f7b
CRBroughton <crbroughton@posteo.uk>
2022-01-23 20:14:18 +0000

    moved logging in logic to App.vue

--- ui/src/pages/index.vue
+++ ui/src/pages/index.vue
@@ -1,28 +1,6 @@
 
 <script setup lang="ts">
-import { useMutation } from '@urql/vue'
-import { useCookie } from 'vue-cookie-next'
-import { Authenticate } from '@/graphql/mutations'
-import { authStore } from '@/store'
 import DefaultLayout from '@/components/DefaultLayout.vue'
-
-const cookie = useCookie()
-const store = authStore()
-const storedCookie = cookie.getCookie('Authorization')
-
-const authenticateUser = async() => {
-  const authenticateMutation = useMutation(Authenticate)
-  await authenticateMutation.executeMutation(storedCookie) // possibly wrong?
-
-  // TODO Must delete local cookie if back end does not find user
-
-  store.logInUser(true)
-}
-
-onMounted(() => {
-  if (!storedCookie) return
-  authenticateUser()
-})
 </script>
 
 <template>
