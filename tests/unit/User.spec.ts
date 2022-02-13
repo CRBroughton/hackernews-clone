cdb362e34ef68140a023de0123d3ee3123c95a69
CRBroughton <crbroughton@posteo.uk>
2022-02-13 19:33:52 +0000

    used mock function in api

--- tests/unit/User.spec.ts
+++ tests/unit/User.spec.ts
@@ -7,14 +7,22 @@ import { uuidGenerator } from '../../src/utils/uuidGenerator'
 let mockCtx: MockContext
 let ctx: Context
 
-const post = {
+const posts = [{
   id: uuidGenerator(),
   createdAt: new Date(),
   description: 'Code-First GraphQL schemas for JavaScript/TypeScript',
   url: 'https://nexusjs.org',
   topic: 'General',
   postedById: '34ce94bf-7019-4a53-8bd1-d0639f06aead',
-}
+},
+{
+  id: uuidGenerator(),
+  createdAt: new Date(),
+  description: 'Code-First GraphQL schemas for JavaScript/TypeScript',
+  url: 'https://nexusjs.org',
+  topic: 'General',
+  postedById: '34ce94bf-7019-4a53-8bd1-d0639f06aesad',
+}]
 
 const user = {
   id: '34ce94bf-7019-4a53-8bd1-d0639f06aead',
@@ -41,9 +49,23 @@ describe('User Tests', () => {
 
   test('returns a users posts ', async() => {
     mockCtx.prisma.user.create.mockResolvedValue(user)
-    mockCtx.prisma.post.create.mockResolvedValue(post)
-    mockCtx.prisma.post.findMany.mockResolvedValue([post])
+    mockCtx.prisma.post.create.mockResolvedValue(posts[0])
+    mockCtx.prisma.post.create.mockResolvedValue(posts[1])
 
-    await expect(userPostQuery(ctx)).resolves.toEqual([post])
+    mockCtx.prisma.post.findMany.mockResolvedValue(posts)
+
+    await expect(userPostQuery(ctx)).resolves.toEqual(posts)
+  })
+
+  test('provides an error for when no userId is present', async() => {
+    mockCtx.prisma.user.create.mockResolvedValue(user)
+    mockCtx.prisma.post.create.mockResolvedValue(posts[0])
+    mockCtx.prisma.post.create.mockResolvedValue(posts[1])
+
+    mockCtx.prisma.post.findMany.mockResolvedValue(posts)
+
+    ctx.userId = ''
+
+    await expect(userPostQuery(ctx)).rejects.toThrow('No userID provided!')
   })
 })
