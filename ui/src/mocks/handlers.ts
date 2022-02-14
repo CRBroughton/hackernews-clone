c8519b7a42c3b6645db6a045356c36e0a6621347
CRBroughton <crbroughton@posteo.uk>
2022-02-14 19:55:11 +0000

    created first msw mock for posts query

--- ui/src/mocks/handlers.ts
+++ ui/src/mocks/handlers.ts
@@ -1,45 +1,76 @@
 // src/mocks/handlers.js
 import { graphql } from 'msw'
+import { uuidGenerator } from '@/utils/uuidGenerator'
 
 export const handlers = [
-  // Handles a "Login" mutation
-  graphql.mutation('Login', (req, res, ctx) => {
-    const { username } = req.variables
-    sessionStorage.setItem('is-authenticated', username)
-
+  graphql.operation((req, res, ctx) => {
+    // const { description } = req.variables
     return res(
       ctx.data({
-        login: {
-          username,
-        },
-      }),
-    )
-  }),
-
-  // Handles a "GetUserInfo" query
-  graphql.query('GetUserInfo', (req, res, ctx) => {
-    const authenticatedUser = sessionStorage.getItem('is-authenticated')
-
-    if (!authenticatedUser) {
-      // When not authenticated, respond with an error
-      return res(
-        ctx.errors([
+        feed: [
           {
-            message: 'Not authenticated',
-            errorType: 'AuthenticationError',
+            id: uuidGenerator(),
+            description: 'Code-First GraphQL schemas for JavaScript/TypeScript',
+            url: 'nexusjs.org',
+            topic: 'General',
+            postedBy: {
+              name: 'crbroughton',
+            },
+            voters: [],
+            __typename: 'Post',
           },
-        ]),
-      )
-    }
-
-    // When authenticated, respond with a query payload
-    return res(
-      ctx.data({
-        user: {
-          username: authenticatedUser,
-          firstName: 'John',
-        },
+          {
+            id: uuidGenerator(),
+            description: 'Code-First GraphQL schemas for JavaScript/TypeScript',
+            url: 'nexusjs.org',
+            topic: 'General',
+            postedBy: {
+              name: 'crbrodughton',
+            },
+            voters: [],
+            __typename: 'Post',
+          }],
       }),
     )
   }),
+  // // Handles a "Login" mutation
+  // graphql.mutation('Login', (req, res, ctx) => {
+  //   const { username } = req.variables
+  //   sessionStorage.setItem('is-authenticated', username)
+
+  //   return res(
+  //     ctx.data({
+  //       login: {
+  //         username,
+  //       },
+  //     }),
+  //   )
+  // }),
+
+  // // Handles a "GetUserInfo" query
+  // graphql.query('GetUserInfo', (req, res, ctx) => {
+  //   const authenticatedUser = sessionStorage.getItem('is-authenticated')
+
+  //   if (!authenticatedUser) {
+  //     // When not authenticated, respond with an error
+  //     return res(
+  //       ctx.errors([
+  //         {
+  //           message: 'Not authenticated',
+  //           errorType: 'AuthenticationError',
+  //         },
+  //       ]),
+  //     )
+  //   }
+
+  //   // When authenticated, respond with a query payload
+  //   return res(
+  //     ctx.data({
+  //       user: {
+  //         username: authenticatedUser,
+  //         firstName: 'John',
+  //       },
+  //     }),
+  //   )
+  // }),
 ]
