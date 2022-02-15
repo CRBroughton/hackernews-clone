2d3be6c8412fe620a6631b0791922d7844801036
CRBroughton <crbroughton@posteo.uk>
2022-02-15 20:08:05 +0000

    fixed api testing setup

--- tests/Post.test.ts
+++ tests/Post.test.ts
@@ -1,32 +1,56 @@
 // tests/Post.test.ts
 import { createTestContext } from './__helpers'
 const ctx = createTestContext()
-it('ensures that a draft can be created and published', async() => {
-  // Create a new draft
+it('Queries the database feed', async() => {
   const feed = await ctx.client.request(`           
     query {
-      feed() {           
+      feed {           
         id
       }
     }
   `)
-  // Snapshot that draft and expect `published` to be false
-  expect(feed).toMatchInlineSnapshot() // 3
+  expect(feed).toMatchInlineSnapshot(`
+    Object {
+      "feed": Array [
+        Object {
+          "id": "99fbda4a-c0cb-458a-b4dc-03858b4a9523",
+        },
+        Object {
+          "id": "570780e5-e4b5-484e-ba6e-df81e0ccd479",
+        },
+        Object {
+          "id": "c48f8db5-3eeb-4c70-b384-70487ac1da9f",
+        },
+      ],
+    }
+`) // 3
   const persistedData = await ctx.prisma.post.findMany()
-  expect(persistedData).toMatchInlineSnapshot()
-//   // Publish the previously created draft
-//   const publishResult = await ctx.client.request(`
-//     mutation publishDraft($draftId: Int!) {
-//       publish(draftId: $draftId) {
-//         id
-//         title
-//         body
-//         published
-//       }
-//     }
-//   `,
-//   { draftId: draftResult.createDraft.id },
-//   )
-//   // Snapshot the published draft and expect `published` to be true
-//   expect(publishResult).toMatchInlineSnapshot()
+  expect(persistedData).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "createdAt": 2022-02-15T19:59:46.274Z,
+        "description": "Code-First GraphQL schemas for JavaScript/TypeScript",
+        "id": "c48f8db5-3eeb-4c70-b384-70487ac1da9f",
+        "postedById": "97527cbe-2de0-40cd-a953-8caae780e66e",
+        "topic": "General",
+        "url": "https://nexusjs.org",
+      },
+      Object {
+        "createdAt": 2022-02-15T19:59:46.274Z,
+        "description": "My Portfolio",
+        "id": "570780e5-e4b5-484e-ba6e-df81e0ccd479",
+        "postedById": "97527cbe-2de0-40cd-a953-8caae780e66e",
+        "topic": "General",
+        "url": "https://crbroughton.me",
+      },
+      Object {
+        "createdAt": 2022-02-15T19:59:46.274Z,
+        "description": "Vite.js",
+        "id": "99fbda4a-c0cb-458a-b4dc-03858b4a9523",
+        "postedById": "97527cbe-2de0-40cd-a953-8caae780e66e",
+        "topic": "General",
+        "url": "https://vitejs.dev",
+      },
+    ]
+`)
 })
--- tests/__helpers.ts
+++ tests/__helpers.ts
@@ -6,7 +6,8 @@ import type { ServerInfo } from 'apollo-server'
 import getPort, { makeRange } from 'get-port'
 import { GraphQLClient } from 'graphql-request'
 import prisma from '../src/client'
-import { server } from '../src'
+import { server } from '../src/server'
+
 type TestContext = {
   client: GraphQLClient
   prisma: PrismaClient
@@ -15,22 +16,26 @@ export function createTestContext(): TestContext {
   const ctx = {} as TestContext
   const graphqlCtx = graphqlTestContext()
   const prismaCtx = prismaTestContext()
+
   beforeEach(async() => {
     const client = await graphqlCtx.before()
-    const db = await prismaCtx.before()
+    const prisma = await prismaCtx.before()
     Object.assign(ctx, {
       client,
-      db,
+      prisma,
     })
   })
+
   afterEach(async() => {
     await graphqlCtx.after()
     await prismaCtx.after()
   })
+
   return ctx
 }
 function graphqlTestContext() {
   let serverInstance: ServerInfo | null = null
+
   return {
     async before() {
       const port = await getPort({ port: makeRange(4000, 6000) })
@@ -41,6 +46,7 @@ function graphqlTestContext() {
       })
       return new GraphQLClient(`http://localhost:${port}`)
     },
+
     async after() {
       serverInstance?.server.close()
     },
@@ -49,6 +55,7 @@ function graphqlTestContext() {
 function prismaTestContext() {
   const prismaBinary = join(__dirname, '..', 'node_modules', '.bin', 'prisma')
   let prismaClient: null | PrismaClient = null
+
   return {
     async before() {
       // Run the migrations to ensure our schema has the required structure
@@ -57,6 +64,7 @@ function prismaTestContext() {
       prismaClient = new PrismaClient()
       return prismaClient
     },
+
     async after() {
       // Release the Prisma Client connection
       await prismaClient?.$disconnect()
