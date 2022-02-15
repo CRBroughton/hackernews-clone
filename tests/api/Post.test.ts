5c6bc6d88b320f50f67570729d54d48cf6cc59d5
CRBroughton <crbroughton@posteo.uk>
2022-02-15 20:39:02 +0000

    moved test snap to folder

--- tests/api/Post.test.ts
+++ tests/api/Post.test.ts
@@ -18,69 +18,7 @@ it('Queries the database feed', async() => {
       }
     }
   `)
-  expect(feed).toMatchInlineSnapshot(`
-    Object {
-      "feed": Array [
-        Object {
-          "description": "Vite.js",
-          "id": "99fbda4a-c0cb-458a-b4dc-03858b4a9523",
-          "postedBy": Object {
-            "id": "97527cbe-2de0-40cd-a953-8caae780e66e",
-            "name": "Alice",
-          },
-          "url": "https://vitejs.dev",
-          "voters": Array [],
-        },
-        Object {
-          "description": "My Portfolio",
-          "id": "570780e5-e4b5-484e-ba6e-df81e0ccd479",
-          "postedBy": Object {
-            "id": "97527cbe-2de0-40cd-a953-8caae780e66e",
-            "name": "Alice",
-          },
-          "url": "https://crbroughton.me",
-          "voters": Array [],
-        },
-        Object {
-          "description": "Code-First GraphQL schemas for JavaScript/TypeScript",
-          "id": "c48f8db5-3eeb-4c70-b384-70487ac1da9f",
-          "postedBy": Object {
-            "id": "97527cbe-2de0-40cd-a953-8caae780e66e",
-            "name": "Alice",
-          },
-          "url": "https://nexusjs.org",
-          "voters": Array [],
-        },
-      ],
-    }
-`)
+  expect(feed).toMatchSnapshot()
   const persistedData = await ctx.prisma.post.findMany()
-  expect(persistedData).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "createdAt": 2022-02-15T19:59:46.274Z,
-        "description": "Code-First GraphQL schemas for JavaScript/TypeScript",
-        "id": "c48f8db5-3eeb-4c70-b384-70487ac1da9f",
-        "postedById": "97527cbe-2de0-40cd-a953-8caae780e66e",
-        "topic": "General",
-        "url": "https://nexusjs.org",
-      },
-      Object {
-        "createdAt": 2022-02-15T19:59:46.274Z,
-        "description": "My Portfolio",
-        "id": "570780e5-e4b5-484e-ba6e-df81e0ccd479",
-        "postedById": "97527cbe-2de0-40cd-a953-8caae780e66e",
-        "topic": "General",
-        "url": "https://crbroughton.me",
-      },
-      Object {
-        "createdAt": 2022-02-15T19:59:46.274Z,
-        "description": "Vite.js",
-        "id": "99fbda4a-c0cb-458a-b4dc-03858b4a9523",
-        "postedById": "97527cbe-2de0-40cd-a953-8caae780e66e",
-        "topic": "General",
-        "url": "https://vitejs.dev",
-      },
-    ]
-`)
+  expect(persistedData).toMatchSnapshot()
 })
