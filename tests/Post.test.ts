63a09a078dad948630b2531e3eada741e86e8b72
CRBroughton <crbroughton@posteo.uk>
2022-02-15 18:41:36 +0000

    installed nexus testing tools

---
+++ tests/Post.test.ts
@@ -0,0 +1,30 @@
+// tests/Post.test.ts
+import { createTestContext } from './__helpers'
+const ctx = createTestContext()
+it('ensures that a draft can be created and published', async() => {
+  // Create a new draft
+  const feed = await ctx.client.request(`           
+    query {
+      feed() {           
+        id
+      }
+    }
+  `)
+  // Snapshot that draft and expect `published` to be false
+  expect(feed).toMatchInlineSnapshot() // 3
+//   // Publish the previously created draft
+//   const publishResult = await ctx.client.request(`
+//     mutation publishDraft($draftId: Int!) {
+//       publish(draftId: $draftId) {
+//         id
+//         title
+//         body
+//         published
+//       }
+//     }
+//   `,
+//   { draftId: draftResult.createDraft.id },
+//   )
+//   // Snapshot the published draft and expect `published` to be true
+//   expect(publishResult).toMatchInlineSnapshot()
+})
