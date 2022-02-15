a10b14f3d7c68c323b0b745503975f7f32e28bd0
CRBroughton <crbroughton@posteo.uk>
2022-02-15 20:12:30 +0000

    improved Post api test

--- tests/api/Post.test.ts
+++ tests/api/Post.test.ts
@@ -1,11 +1,20 @@
 // tests/Post.test.ts
-import { createTestContext } from './__helpers'
+import { createTestContext } from '../__helpers'
 const ctx = createTestContext()
 it('Queries the database feed', async() => {
   const feed = await ctx.client.request(`           
     query {
       feed {           
         id
+        description
+        url
+        postedBy {
+          id
+          name
+        }
+        voters {
+          id
+        }
       }
     }
   `)
@@ -13,17 +22,38 @@ it('Queries the database feed', async() => {
     Object {
       "feed": Array [
         Object {
+          "description": "Vite.js",
           "id": "99fbda4a-c0cb-458a-b4dc-03858b4a9523",
+          "postedBy": Object {
+            "id": "97527cbe-2de0-40cd-a953-8caae780e66e",
+            "name": "Alice",
+          },
+          "url": "https://vitejs.dev",
+          "voters": Array [],
         },
         Object {
+          "description": "My Portfolio",
           "id": "570780e5-e4b5-484e-ba6e-df81e0ccd479",
+          "postedBy": Object {
+            "id": "97527cbe-2de0-40cd-a953-8caae780e66e",
+            "name": "Alice",
+          },
+          "url": "https://crbroughton.me",
+          "voters": Array [],
         },
         Object {
+          "description": "Code-First GraphQL schemas for JavaScript/TypeScript",
           "id": "c48f8db5-3eeb-4c70-b384-70487ac1da9f",
+          "postedBy": Object {
+            "id": "97527cbe-2de0-40cd-a953-8caae780e66e",
+            "name": "Alice",
+          },
+          "url": "https://nexusjs.org",
+          "voters": Array [],
         },
       ],
     }
-`) // 3
+`)
   const persistedData = await ctx.prisma.post.findMany()
   expect(persistedData).toMatchInlineSnapshot(`
     Array [
