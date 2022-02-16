854b66f0f7156601ade19ad4a382430dc4434bda
CRBroughton <crbroughton@posteo.uk>
2022-02-16 21:34:48 +0000

    created GetUserPosts test

--- tests/api/Post.test.ts
+++ tests/api/Post.test.ts
@@ -1,23 +1,24 @@
 // tests/Post.test.ts
 import { createTestContext } from '../__helpers'
 const ctx = createTestContext()
+
 it('Queries the database feed', async() => {
   const feed = await ctx.client.request(`           
-    query {
-      feed {           
-        id
-        description
-        url
-        postedBy {
+      query {
+        feed {           
           id
-          name
-        }
-        voters {
-          id
+          description
+          url
+          postedBy {
+            id
+            name
+          }
+          voters {
+            id
+          }
         }
       }
-    }
-  `)
+    `)
   expect(feed).toMatchSnapshot()
   const persistedData = await ctx.prisma.post.findMany()
   expect(persistedData).toMatchSnapshot()
