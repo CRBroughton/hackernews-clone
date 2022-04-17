3ecf213825d3be49ba359d8d537f6a3d3a7e4a27
CRBroughton <crbroughton@posteo.uk>
2022-04-17 08:49:22 +0100

    removed jest, removed vuelidate code

--- ui/src/pages/create/index.vue
+++ ui/src/pages/create/index.vue
@@ -1,38 +1,15 @@
 <script setup lang="ts">
-import { required, url } from '@vuelidate/validators'
-import { useVuelidate } from '@vuelidate/core'
 import { useMutation } from 'villus'
 import { Create } from '@/graphql/mutations'
-import { authStore } from '@/store'
 import type { Post } from '@/Types'
 
 const router = useRouter()
-const store = authStore()
 
 const { execute } = useMutation(Create)
 const networkResponse = ref()
 
-const rules = {
-  createPost: {
-    url: { required, url },
-    topic: { required },
-    description: { required },
-  },
-}
-
-const v$ = useVuelidate(rules, store)
-
-const errors = computed (() => {
-  networkResponse.value = ''
-  return v$.value.$errors
-})
-
 const createPost = async({ ...Post }: Post) => {
   const variables = Post
-
-  const isFormCorrect = await v$.value.$validate()
-
-  if (!isFormCorrect) return
 
   execute(variables).then(async(result) => {
     if (result.error)
@@ -54,9 +31,6 @@ const createPost = async({ ...Post }: Post) => {
       <div class="flex flex-col fixed bottom-0 right-0">
         <div v-if="networkResponse?.error">
           <Notification :text="networkResponse.error.toString().slice(9)" />
-        </div>
-        <div v-for="error in errors" :key="error.$uid">
-          <Notification :text="`${error.$property} - ${error.$message.toString().toLowerCase()}`" />
         </div>
       </div>
     </div>
