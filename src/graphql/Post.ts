3b73e7efc9caef10b6af5bd558187896d47c1a0a
CRBroughton <crbroughton@posteo.uk>
2022-05-21 14:49:18 +0100

    refactor(api): refactored post functions

--- src/graphql/Post.ts
+++ src/graphql/Post.ts
@@ -1,4 +1,4 @@
-import { extendType, list, nonNull, objectType, queryField, stringArg } from 'nexus'
+import { list, mutationField, nonNull, objectType, queryField, stringArg } from 'nexus'
 
 export const Post = objectType({
   name: 'Post',
@@ -52,87 +52,77 @@ export const TopicQuery = queryField('getTopicPosts', {
   },
 })
 
-export const PostMutation = extendType({
-  type: 'Mutation',
-  definition(t) {
-    t.nonNull.field('post', {
-      type: 'Post',
-      args: {
-        description: nonNull(stringArg()),
-        url: nonNull(stringArg()),
-        topic: nonNull(stringArg()),
-      },
+export const post = mutationField('post', {
+  type: Post,
+  args: {
+    description: nonNull(stringArg()),
+    url: nonNull(stringArg()),
+    topic: nonNull(stringArg()),
+  },
 
-      async resolve(_parent, args, context) {
-        const { description, url } = args
-        let topic = args.topic
+  async resolve(_parent, args, context) {
+    const { description, url } = args
+    let topic = args.topic
 
-        if (!args.description || !args.url)
-          throw new Error('Missing a required field!')
+    if (!args.description || !args.url)
+      throw new Error('Missing a required field!')
 
-        if (!args.topic)
-          topic = 'General'
+    if (!args.topic)
+      topic = 'General'
 
-        if (!context.userId)
-          throw new Error('Cannot post without logging in.')
+    if (!context.userId)
+      throw new Error('Cannot post without logging in.')
 
-        const user = await context.prisma.user.findUnique({
-          where: { id: context.userId },
-        })
+    const user = await context.prisma.user.findUnique({
+      where: { id: context.userId },
+    })
 
-        if (user?.banned)
-          throw new Error(`You are banned! \r\n Reason: ${user.banReason}`)
+    if (user?.banned)
+      throw new Error(`You are banned! \r\n Reason: ${user.banReason}`)
 
-        const newPost = context.prisma.post.create({
-          data: {
-            description,
-            url,
-            topic,
-            postedBy: { connect: { id: context.userId } },
-          },
-        })
-
-        return newPost
+    const newPost = context.prisma.post.create({
+      data: {
+        description,
+        url,
+        topic,
+        postedBy: { connect: { id: context.userId } },
       },
     })
+
+    return newPost
   },
 })
 
-export const deletePost = extendType({
-  type: 'Mutation',
-  definition(t) {
-    t.nonNull.field('deletePost', {
-      type: 'ID',
-      args: {
-        id: nonNull(stringArg()),
+export const deletePost = mutationField('deletePost', {
+  type: ID,
+  args: {
+    id: nonNull(stringArg()),
+  },
+
+  async resolve(_parent, args, context) {
+    const getPostedUser = await context.prisma.post.findUnique({
+      where: {
+        id: args.id,
       },
-
-      async resolve(_parent, args, context) {
-        const getPostedUser = await context.prisma.post.findUnique({
-          where: {
-            id: args.id,
-          },
-          select: {
-            postedById: true,
-          },
-        })
-
-        if (!context.userId)
-          throw new Error('Cannot post without logging in.')
-
-        if (!getPostedUser?.postedById)
-          throw new Error('Post does not exist!')
-
-        if (getPostedUser?.postedById !== context.userId)
-          throw new Error('Cannot delete posts that you dont own!')
-
-        await context.prisma.post.delete({
-          where: {
-            id: args.id,
-          },
-        })
-        return `deleted post ${args.id}`
+      select: {
+        postedById: true,
       },
     })
+
+    if (!context.userId)
+      throw new Error('Cannot post without logging in.')
+
+    if (!getPostedUser?.postedById)
+      throw new Error('Post does not exist!')
+
+    if (getPostedUser?.postedById !== context.userId)
+      throw new Error('Cannot delete posts that you dont own!')
+
+    await context.prisma.post.delete({
+      where: {
+        id: args.id,
+      },
+    })
+    return `deleted post ${args.id}`
   },
 })
