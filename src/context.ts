440031b5afdfae51606b5b2b42eb082495287a12
CRBroughton <crbroughton@posteo.uk>
2022-05-25 18:30:35 +0100

    chore(context): removed old apollo context

--- src/context.ts
+++ src/context.ts
@@ -1,7 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-import type { Request } from 'express'
-import type { DeepMockProxy } from 'jest-mock-extended'
-import { mockDeep } from 'jest-mock-extended'
 import { decodeAuthHeader } from './utils/auth'
 export const prisma = new PrismaClient()
 
@@ -10,19 +7,7 @@ export interface Context {
   userId?: string
 }
 
-export type MockContext = {
-  prisma: DeepMockProxy<PrismaClient>
-  userId?: string
-}
-
-export const createMockContext = (): MockContext => {
-  return {
-    prisma: mockDeep<PrismaClient>(),
-    userId: '34ce94bf-7019-4a53-8bd1-d0639f06aead',
-  }
-}
-
-export const context = ({ req }: { req: Request }): Context => {
+export const context = ({ req }: { req: any }): Context => {
   const storedCookie = req.headers.authorization as string
   const token
       = req && req.headers.authorization
